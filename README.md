7ae173c98cb4da62923da2d22598837df5d907b3
GuGustavu <136768202+GuGustavu@users.noreply.github.com>
2024-12-14 14:03:30 +0000

    Adicionado script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinar o nova linguagem.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Pagina inicial</h1>
+function Home() {
+  return <h1>Pagina inicial</h1>;
 }
 
-export default Home 
+function teste() {
+  console.log("teste");
+}
+
+export default Home;
